b9d197963cb9d4ae9236038127c4ed1bf6dae549
Jacob Allen <enoua5@gmail.com>
2017-01-23 13:59:45 -0700

    Update todo.js

--- GameElements/data/todo.js
+++ GameElements/data/todo.js
@@ -3,7 +3,6 @@ var todos=[
 "Add one-way blocks",
 "Add ice blocks",
 "Confirm compatibility with Firefox",
-"Make minigame have same controls as normal mode",
 "Add modding guide to offline version",
 "Add levels implementing new blocks",
 "Add 'restart level' option"
