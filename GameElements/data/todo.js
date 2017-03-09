104a42412d596dcef5267629dbee6cf7d9c73395
Jacob Allen <enoua5@gmail.com>
2017-03-09 13:50:57 -0700

    completed back and reset buttons

--- GameElements/data/todo.js
+++ GameElements/data/todo.js
@@ -1,10 +1,8 @@
 var todos=[
-"Add back button",
 "Add portals",
 "Add one-way blocks",
 "Add ice blocks",
 "Confirm compatibility with Firefox",
 "Add modding guide to offline version",
-"Add levels implementing new blocks",
-"Add 'restart level' option"
+"Add levels implementing new blocks"
 ]
