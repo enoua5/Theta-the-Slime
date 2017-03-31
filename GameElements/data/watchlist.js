9d78ce9f4cddbccd21c26d993c80942a9f171705
Jacob Allen <enoua5@gmail.com>
2017-03-31 12:59:25 -0600

    Update watchlist.js

--- GameElements/data/watchlist.js
+++ GameElements/data/watchlist.js
@@ -5,5 +5,6 @@ var watchlist=
 {resolved: 1, content: "Ghostly spikes visable in some levels"},
 {resolved: 1, content: "Coins appear in front of score"},
 {resolved: 1, content: "Side spikes in the middle of the level have a cut off base"},
-{resolved: 1, content: "I broke the level editor"}
+{resolved: 1, content: "I broke the level editor"},
+{resolved: 0, content: "Chaos does not know about the new spike format"}
 ]
