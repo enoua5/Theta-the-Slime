2e0e0ab8c65011acdf8fe28dfd725b1061266134
Jacob Allen <enoua5@gmail.com>
2017-05-10 17:13:45 -0600

    4 -> 9

--- GameElements/js/audio.js
+++ GameElements/js/audio.js
@@ -1,5 +1,5 @@
-var intro = new Audio('assets/sounds/music/4/intro.ogg');
-var loop = new Audio('assets/sounds/music/4/loop.ogg');
+var intro = new Audio('assets/sounds/music/9/intro.ogg');
+var loop = new Audio('assets/sounds/music/9/loop.ogg');
 intro.play();
 intro.addEventListener("ended", function () {
     intro.pause();
