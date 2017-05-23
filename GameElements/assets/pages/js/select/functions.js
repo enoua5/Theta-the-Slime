ef4d41cd4e5d5cde5f0791cb0e7f290cd6dd704c
Jacob Allen <enoua5@gmail.com>
2017-05-23 14:50:28 -0600

    Fixing lots of stupidity surrounding coins

--- GameElements/assets/pages/js/select/functions.js
+++ GameElements/assets/pages/js/select/functions.js
@@ -32,3 +32,31 @@ function resetSave()
 	localStorage.reloaded="0";
 	location.reload();
 }
+function getFullCoins(pack)
+{
+	var ret=new Array(pack.length)
+	for(var i=0; i<pack.length; i++)
+	{
+		var tot=0;
+		for(var x=0; x<pack[i].data.length; x++)
+			for(var y=0; y<pack[i].data[x].length; y++)
+			{
+				var id=0;
+				if(typeof(pack[i].data[x][y])=="number")
+					id=pack[i].data[x][y];
+				else if(typeof(pack[i].data[x][y])=="object")
+					id=pack[i].data[x][y].id;
+				if(id==8)
+					tot++;
+			}
+		ret[i]=tot;
+	}
+	return ret;
+}
+function sum(arr)
+{
+	var tot=0;
+	for(var i=0; i<arr.length; i++)
+		tot+=arr[i];
+	return tot;
+}
--- GameElements/assets/pages/js/select/select.js
+++ GameElements/assets/pages/js/select/select.js
@@ -4,34 +4,14 @@ function displaySelect()
 	//just making it easier to add more later
 	var x=0;
 	var y = 0;
-	if (requestedPack == "?premade") {
-        console.log("loading premade pack")
-	    for (i = 0; i < levels.length; i++) {
+	if (requestedPack == "?premade" ||
+		requestedPack == "?premade"||
+		requestedPack=="?special")
+	{
+	    levels=packs[requestedPack.split("?")[1]];
+	    for (i = 0; i < levels.length; i++)
+	    {
 	        box.innerHTML += "<div class='level' style='top:" + ((y * 250) + 100) + "px;left:" + ((x * 250) + 250) + "px;' onClick='localStorage.reloaded=\"0\";location.assign(\"player.html\"+location.search+\"&"+i+"\")'>" + (i + 1) + "</div><div  class='stat' id='stats" + i + "' style='top:" + ((y * 250) + 200) + "px;left:" + ((x * 250) + 250) + "px; position: absolute;'><div style='display:inline-block; background:url(" + path + "/chars/0/splat1.png); width: 15px; height: 15px; position: absolute; top: 5px; left:5px; background-size: contain'></div><span id='deathCount" + i + "'>" + d[i] + "</span><br><div style='display:inline-block;position: absolute; left: 5px; top: 20px; background:url(" + path + "/stageimg/coin.gif); height: 15px; width: 15px;background-size:contain'></div><span id='coinCount" + i + "'>" + c[i] + "/"+maxc1[i]+"</span></div>";
-	        x++;
-	        if (x > 2) {
-	            x = 0;
-	            y++;
-	        }
-	    }
-	}
-	if (requestedPack == "?bonus")
-	{
-        console.log("loading bonus pack")
-	    for (i = 0; i < levels.length; i++) {
-	        box.innerHTML += "<div class='level' style='top:" + ((y * 250) + 100) + "px;left:" + ((x * 250) + 250) + "px;' onClick='launchLevel(" + i + ")'>" + (i + 1) + "</div><div  class='stat' id='stats" + i + "' style='top:" + ((y * 250) + 200) + "px;left:" + ((x * 250) + 250) + "px; position: absolute;'><div style='display:inline-block; background:url(" + path + "/chars/0/splat1.png); width: 15px; height: 15px; position: absolute; top: 5px; left:5px; background-size: contain'></div><span id='deathCount" + i + "'>" + db[i] + "</span><br><div style='display:inline-block;position: absolute; left: 5px; top: 20px; background:url(" + path + "/stageimg/coin.gif); height: 15px; width: 15px;background-size:contain'></div><span id='coinCount" + i + "'>" + cb[i] + "/" + maxc2[i] + "</span></div>";
-	        x++;
-	        if (x > 2) {
-	            x = 0;
-	            y++;
-	        }
-	    }
-	}
-	if (requestedPack == "?special")
-	{
-	    console.log("loading special pack")
-	    for (i = 0; i < levels.length; i++) {
-	        box.innerHTML += "<div class='level' style='top:" + ((y * 250) + 100) + "px;left:" + ((x * 250) + 250) + "px;' onClick='launchLevel(" + i + ")'>" + (i + 1) + "</div><div  class='stat' id='stats" + i + "' style='top:" + ((y * 250) + 200) + "px;left:" + ((x * 250) + 250) + "px; position: absolute;'><div style='display:inline-block; background:url(" + path + "/chars/0/splat1.png); width: 15px; height: 15px; position: absolute; top: 5px; left:5px; background-size: contain'></div><span id='deathCount" + i + "'>" + ds[i] + "</span><br><div style='display:inline-block;position: absolute; left: 5px; top: 20px; background:url(" + path + "/stageimg/coin.gif); height: 15px; width: 15px;background-size:contain'></div><span id='coinCount" + i + "'>" + cs[i] + "/" + maxc3[i] + "</span></div>";
 	        x++;
 	        if (x > 2) {
 	            x = 0;
@@ -41,9 +21,9 @@ function displaySelect()
 	}
 	if (requestedPack == "?custom")
 	{
-        console.log("loading custom launcher")
+	    console.log(levels)
 	    for (i = 0; i < levels.length; i++) {
-	        box.innerHTML += "<div class='level' style='top:" + ((y * 250) + 100) + "px;left:" + ((x * 250) + 250) + "px;' onClick='launchLevel(" + i + ")'>" + (i + 1) + "</div><div  class='stat' id='update" + i + "' style='top:" + ((y * 250) + 200) + "px;left:" + ((x * 250) + 250) + "px; position: absolute;' onClick='uploadCustom(" + i + ")'>upload level</div>";
+	        box.innerHTML += "<div class='level' style='top:" + ((y * 250) + 100) + "px;left:" + ((x * 250) + 250) + "px;' onClick='localStorage.reloaded=\"0\";location.assign(\"player.html\"+location.search+\"&"+i+"\")'>" + (i + 1) + "</div><div  class='stat' id='update" + i + "' style='top:" + ((y * 250) + 200) + "px;left:" + ((x * 250) + 250) + "px; position: absolute;' onClick='uploadCustom(" + i + ")'>upload level</div>";
 	        x++;
 	        if (x > 2) {
 	            x = 0;
