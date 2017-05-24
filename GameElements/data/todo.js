64c21d6b9c74db74188c63454471e1e9d46d6082
Jacob Allen <enoua5@gmail.com>
2017-05-24 17:45:40 -0600

    Update todo.js

--- GameElements/data/todo.js
+++ GameElements/data/todo.js
@@ -1,7 +1,4 @@
 var todos=[
-"Replace that one last song",
-"Fix the trail off at the end of intros",
-"Make the back button disapear in levels",
 "Create new level editor",
 "Add portals",
 "Add one-way blocks",
