06e3ef1089491c8787912a7319e4f3bdb3cb119f
Jacob Allen <enoua5@gmail.com>
2017-08-13 11:10:09 -0600

    Update todo.js

--- GameElements/data/todo.js
+++ GameElements/data/todo.js
@@ -1,8 +1,8 @@
 var todos=[
-"Create new level editor",
-"Add portals",
-"Add one-way blocks",
-"Add ice blocks",
-"Add modding guide to offline version",
-"Add levels implementing new blocks"
+  "Create new level editor",
+  "Add portals",
+  "Add one-way blocks",
+  "Add ice blocks",
+  "Add modding guide to offline version",
+  "Add levels implementing new blocks"
 ]
