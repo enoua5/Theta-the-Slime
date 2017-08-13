6c58e5aa01712dddcdcede12215f15bcd342263a
Jacob Allen <enoua5@gmail.com>
2017-08-13 11:09:50 -0600

    remove joke entry

--- GameElements/data/watchlist.js
+++ GameElements/data/watchlist.js
@@ -1,12 +1,11 @@
 var watchlist=
 [
-{resolved: 1, content: "Download button missing its file"},
-{resolved: 1, content: "Water exhibiting launching behavior"},
-{resolved: 1, content: "Ghostly spikes visable in some levels"},
-{resolved: 1, content: "Coins appear in front of score"},
-{resolved: 1, content: "Side spikes in the middle of the level have a cut off base"},
-{resolved: 1, content: "I broke the level editor"},
-{resolved: 0, content: "Chaos does not know about the new spike format"},
-{resolved: 0, content: "Spikes don't scale correctly at different resolutions"},
-{resolved: 0, content: "Everything is broken."}
+  {resolved: 1, content: "Download button missing its file"},
+  {resolved: 1, content: "Water exhibiting launching behavior"},
+  {resolved: 1, content: "Ghostly spikes visable in some levels"},
+  {resolved: 1, content: "Coins appear in front of score"},
+  {resolved: 1, content: "Side spikes in the middle of the level have a cut off base"},
+  {resolved: 1, content: "I broke the level editor"},
+  {resolved: 0, content: "Chaos does not know about the new spike format"},
+  {resolved: 0, content: "Spikes don't scale correctly at different resolutions"}
 ]
