47d057de39b5e90fd5f4a682f71c4352e55ff755
Jacob Allen <enoua5@gmail.com>
2017-09-30 12:54:04 -0600

    Fix the loading of level data.

--- GameElements/assets/pages/js/select/select.js
+++ GameElements/assets/pages/js/select/select.js
@@ -5,13 +5,28 @@ function displaySelect()
 	var x=0;
 	var y = 0;
 	if (requestedPack == "?premade" ||
-		requestedPack == "?premade"||
+		requestedPack == "?bonus"||
 		requestedPack=="?special")
 	{
 	    levels=packs[requestedPack.split("?")[1]];
 	    for (i = 0; i < levels.length; i++)
 	    {
-	        box.innerHTML += "<div class='level' style='top:" + ((y * 250) + 100) + "px;left:" + ((x * 250) + 250) + "px;' onClick='localStorage.reloaded=\"0\";location.assign(\"player.html\"+location.search+\"&"+i+"\")'>" + (i + 1) + "</div><div  class='stat' id='stats" + i + "' style='top:" + ((y * 250) + 200) + "px;left:" + ((x * 250) + 250) + "px; position: absolute;'><div style='display:inline-block; background:url(" + path + "/chars/0/splat1.png); width: 15px; height: 15px; position: absolute; top: 5px; left:5px; background-size: contain'></div><span id='deathCount" + i + "'>" + d[i] + "</span><br><div style='display:inline-block;position: absolute; left: 5px; top: 20px; background:url(" + path + "/stageimg/coin.gif); height: 15px; width: 15px;background-size:contain'></div><span id='coinCount" + i + "'>" + c[i] + "/"+maxc1[i]+"</span></div>";
+		d={
+			"?premade":d,
+			"?bonus":db,
+			"?special":ds
+		}[requestedPack];
+		c={
+			"?premade":c,
+			"?bonus":cb,
+			"?special":cs
+		}[requestedPack];
+		maxc={
+			"?premade":maxc1,
+			"?bonus":maxc2,
+			"?special":maxc3
+		}[requestedPack];
+	        box.innerHTML += "<div class='level' style='top:" + ((y * 250) + 100) + "px;left:" + ((x * 250) + 250) + "px;' onClick='localStorage.reloaded=\"0\";location.assign(\"player.html\"+location.search+\"&"+i+"\")'>" + (i + 1) + "</div><div  class='stat' id='stats" + i + "' style='top:" + ((y * 250) + 200) + "px;left:" + ((x * 250) + 250) + "px; position: absolute;'><div style='display:inline-block; background:url(" + path + "/chars/0/splat1.png); width: 15px; height: 15px; position: absolute; top: 5px; left:5px; background-size: contain'></div><span id='deathCount" + i + "'>" + d[i] + "</span><br><div style='display:inline-block;position: absolute; left: 5px; top: 20px; background:url(" + path + "/stageimg/coin.gif); height: 15px; width: 15px;background-size:contain'></div><span id='coinCount" + i + "'>" + c[i] + "/"+maxc[i]+"</span></div>";
 	        x++;
 	        if (x > 2) {
 	            x = 0;
