598a30014cc110e385ae4330005b4b48a42a1bd7
Jacob Allen <enoua5@gmail.com>
2017-10-23 19:31:29 -0600

    fix localStorage

--- GameElements/assets/pages/js/player/pause.js
+++ GameElements/assets/pages/js/player/pause.js
@@ -5,15 +5,15 @@ function pause()
 	document.getElementById("loadingScreen").style.width="1200px";
 	screen=document.getElementById("loadingScreen");
 	screen.innerHTML="<h1 style='color: white'>PAUSED</h1>";
-	screen.innerHTML+="<h3 style='color: white;'>music: </h3><input type='range' id='myRange' value='"+localStorage.e5_googame_music_volume+"' min='0' max='1' step='0.01' onchange='localStorage.e5_googame_music_volume=this.value; updateMusic();'>";
-	screen.innerHTML+="<h3 style='color: white;'>sfx: </h3><input type='range' id='myRange' value='"+localStorage.e5_googame_sfx_volume+"' min='0' max='1' step='0.01' onchange='localStorage.e5_googame_sfx_volume=this.value; updateSfx();'>";
+	screen.innerHTML+="<h3 style='color: white;'>music: </h3><input type='range' id='myRange' value='"+localStorage.getItem("e5_googame_music_volume")+"' min='0' max='1' step='0.01' onchange='localStorage.setItem(\"e5_googame_music_volume\",this.value); updateMusic();'>";
+	screen.innerHTML+="<h3 style='color: white;'>sfx: </h3><input type='range' id='myRange' value='"+localStorage.getItem("e5_googame_sfx_volume")+"' min='0' max='1' step='0.01' onchange='localStorage.setItem(\"e5_googame_sfx_volume\",this.value); updateSfx();'>";
 	screen.innerHTML+="<h2 onClick='resume()' style='color: white'>back to game</h2>";
 	screen.innerHTML+="<h2 onClick='reset()' style='color: white'>reset level</h2>";
 	screen.innerHTML+="<h2 onClick='toSelect()' style='color: white'>quit</h2>";
 }
 function toSelect()
 {
-	localStorage.reloaded = "0";
+	localStorage.setItem("reloaded", "0");
 	location.assign("select.html"+requestedPack);
 }
 function resume()
@@ -23,6 +23,6 @@ function resume()
 }
 function reset()
 {
-	localStorage.reloaded = "0";
+	localStorage.setItem("reloaded", "0");
 	location.reload();
 }
