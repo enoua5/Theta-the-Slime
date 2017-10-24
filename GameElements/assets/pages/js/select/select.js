900db0500c1c96105e507a4306a9f56f751988d7
Jacob Allen <enoua5@gmail.com>
2017-10-23 19:28:37 -0600

    fix localStorage

--- GameElements/assets/pages/js/select/select.js
+++ GameElements/assets/pages/js/select/select.js
@@ -26,7 +26,7 @@ function displaySelect()
 			"?bonus":maxc2,
 			"?special":maxc3
 		}[requestedPack];
-	        box.innerHTML += "<div class='level' style='top:" + ((y * 250) + 100) + "px;left:" + ((x * 250) + 250) + "px;' onClick='localStorage.reloaded=\"0\";location.assign(\"player.html\"+location.search+\"&"+i+"\")'>" + (i + 1) + "</div><div  class='stat' id='stats" + i + "' style='top:" + ((y * 250) + 200) + "px;left:" + ((x * 250) + 250) + "px; position: absolute;'><div style='display:inline-block; background:url(" + path + "/chars/0/splat1.png); width: 15px; height: 15px; position: absolute; top: 5px; left:5px; background-size: contain'></div><span id='deathCount" + i + "'>" + d[i] + "</span><br><div style='display:inline-block;position: absolute; left: 5px; top: 20px; background:url(" + path + "/stageimg/coin.gif); height: 15px; width: 15px;background-size:contain'></div><span id='coinCount" + i + "'>" + c[i] + "/"+maxc[i]+"</span></div>";
+	        box.innerHTML += "<div class='level' style='top:" + ((y * 250) + 100) + "px;left:" + ((x * 250) + 250) + "px;' onClick='localStorage.setItem(\"reloaded\",\"0\");location.assign(\"player.html\"+location.search+\"&"+i+"\")'>" + (i + 1) + "</div><div  class='stat' id='stats" + i + "' style='top:" + ((y * 250) + 200) + "px;left:" + ((x * 250) + 250) + "px; position: absolute;'><div style='display:inline-block; background:url(" + path + "/chars/0/splat1.png); width: 15px; height: 15px; position: absolute; top: 5px; left:5px; background-size: contain'></div><span id='deathCount" + i + "'>" + d[i] + "</span><br><div style='display:inline-block;position: absolute; left: 5px; top: 20px; background:url(" + path + "/stageimg/coin.gif); height: 15px; width: 15px;background-size:contain'></div><span id='coinCount" + i + "'>" + c[i] + "/"+maxc[i]+"</span></div>";
 	        x++;
 	        if (x > 2) {
 	            x = 0;
@@ -38,7 +38,7 @@ function displaySelect()
 	{
 	    console.log(levels)
 	    for (i = 0; i < levels.length; i++) {
-	        box.innerHTML += "<div class='level' style='top:" + ((y * 250) + 100) + "px;left:" + ((x * 250) + 250) + "px;' onClick='localStorage.reloaded=\"0\";location.assign(\"player.html\"+location.search+\"&"+i+"\")'>" + (i + 1) + "</div><div  class='stat' id='update" + i + "' style='top:" + ((y * 250) + 200) + "px;left:" + ((x * 250) + 250) + "px; position: absolute;' onClick='uploadCustom(" + i + ")'>upload level</div>";
+	        box.innerHTML += "<div class='level' style='top:" + ((y * 250) + 100) + "px;left:" + ((x * 250) + 250) + "px;' onClick='localStorage.setItem(\"reloaded\",\"0\");location.assign(\"player.html\"+location.search+\"&"+i+"\")'>" + (i + 1) + "</div><div  class='stat' id='update" + i + "' style='top:" + ((y * 250) + 200) + "px;left:" + ((x * 250) + 250) + "px; position: absolute;' onClick='uploadCustom(" + i + ")'>upload level</div>";
 	        x++;
 	        if (x > 2) {
 	            x = 0;
@@ -53,6 +53,6 @@ function displaySelect()
 }
 function toSelect()
 {
-	localStorage.reloaded="0";
+	localStorage.setItem("reloaded","0");
 	location.reload();
 }
