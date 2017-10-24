ea691c3a1e782df5b8d97b428dff94274335e18d
Jacob Allen <enoua5@gmail.com>
2017-10-23 19:11:08 -0600

    fix localStorage

--- GameElements/assets/pages/js/select/settings.js
+++ GameElements/assets/pages/js/select/settings.js
@@ -4,26 +4,26 @@ maxc3=getFullCoins(packs.special);
 function uploadCustom(id) {
     newLevelData = prompt("paste level data here");
     if (newLevelData !== "" && newLevelData !== null) {
-        localStorage["e5_goo_c" + (id + 1)] = newLevelData;
+        localStorage.setItem("e5_goo_c" + (id + 1), newLevelData);
     }
     try
     {
-        levels[id] = eval("(" + localStorage["e5_goo_c" + (id + 1)] + ")");
+        levels[id] = eval("(" + localStorage.getItem("e5_goo_c" + (id + 1)) + ")");
         alert("upload success!");
     }
     catch(err)
     {
         alert("upload failed!");
-        delete localStorage["e5_goo_c" + (id + 1)];
-        localStorage.reloaded = 0;
+        localStorage.removeItem("e5_goo_c" + (id + 1));
+        localStorage.setItem("reloaded", 0);
         window.location.reload();
     }
 }
 requestedPack=location.search;
-if(!localStorage.e5_googame_sfx_volume)
-	localStorage.e5_googame_sfx_volume="0.25";
-if(!localStorage.e5_googame_music_volume)
-	localStorage.e5_googame_music_volume="0.25";
+if(!localStorage.getItem("e5_googame_sfx_volume"))
+	localStorage.setItem("e5_googame_sfx_volume","0.25");
+if(!localStorage.getItem("e5_googame_music_volume"))
+	localStorage.setItem("e5_googame_music_volume","0.25");
 function addLevel(levelObject)
 {
 	if(levelObject!==undefined)
