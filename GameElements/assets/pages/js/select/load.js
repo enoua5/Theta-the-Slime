507a22f23935e941175242add8843f34c84fa590
Jacob Allen <enoua5@gmail.com>
2017-10-23 19:25:37 -0600

    fix localStorage

--- GameElements/assets/pages/js/select/load.js
+++ GameElements/assets/pages/js/select/load.js
@@ -1,73 +1,73 @@
 //load save data
-if(localStorage.e5_goo_deaths===undefined || localStorage.e5_goo_deaths==="")
+if(localStorage.getItem("e5_goo_deaths")===null || localStorage.getItem("e5_goo_deaths")==="")
 {
 	var asdf="";
 	for(n=0; n<levels.length; n++)
 	{
 		asdf+=String.fromCharCode(255)
 	}
-	localStorage.e5_goo_deaths=asdf
+	localStorage.setItem("e5_goo_deaths",asdf);
 }
-if(localStorage.e5_goo_coins===undefined || localStorage.e5_goo_coins==="")
+if(localStorage.getItem("e5_goo_coins")===null || localStorage.getItem("e5_goo_coins")==="")
 {
 	var asdf="";
 	for(n=0; n<levels.length; n++)
 	{
 		asdf+=String.fromCharCode(0)
 	}
-	localStorage.e5_goo_coins=asdf
+	localStorage.setItem("e5_goo_coins",asdf);
 }
-if (localStorage.e5_goo_deathsB === undefined || localStorage.e5_goo_deathsB==="") {
+if (localStorage.getItem("e5_goo_deathsB")===null || localStorage.getItem("e5_goo_deathsB")==="") {
     var asdf = "";
     for (n = 0; n < levels.length; n++) {
         asdf += String.fromCharCode(255)
     }
-    localStorage.e5_goo_deathsB = asdf;
+    localStorage.setItem("e5_goo_deathsB", asdf);
 }
-if (localStorage.e5_goo_coinsB === undefined || localStorage.e5_goo_coinsB==="") {
+if (localStorage.getItem("e5_goo_coinsB") === null || localStorage.getItem("e5_goo_coinsB")==="") {
     var asdf = "";
     for (n = 0; n < levels.length; n++) {
         asdf += String.fromCharCode(0)
     }
-    localStorage.e5_goo_coinsB = asdf;
+    localStorage.setItem("e5_goo_coinsB", asdf);
 }
-if (localStorage.e5_goo_deathsS === undefined || localStorage.e5_goo_deathsS==="") {
+if (localStorage.getItem("e5_goo_deathsS") === null || localStorage.getItem("e5_goo_deathsS")==="") {
     var asdf = "";
     for (n = 0; n < levels.length; n++) {
         asdf += String.fromCharCode(255)
     }
-    localStorage.e5_goo_deathsS = asdf;
+    localStorage.setItem("e5_goo_deathsS", asdf);
 }
-if (localStorage.e5_goo_coinsS === undefined || localStorage.e5_goo_coinsS==="") {
+if (localStorage.getItem("e5_goo_coinsS") === null || localStorage.getItem("e5_goo_coinsS")==="") {
     var asdf = "";
     for (n = 0; n < levels.length; n++) {
         asdf += String.fromCharCode(0)
     }
-    localStorage.e5_goo_coinsS = asdf;
+    localStorage.setItem("e5_goo_coinsS", asdf);
 }
 d=[];
 c=[];
-for(n=0; n<localStorage.e5_goo_deaths.length; n++)
+for(n=0; n<localStorage.getItem("e5_goo_deaths").length; n++)
 {
-	d[n]=localStorage.e5_goo_deaths[n].charCodeAt()
+	d[n]=localStorage.getItem("e5_goo_deaths")[n].charCodeAt()
 }
-for(n=0; n<localStorage.e5_goo_coins.length; n++)
+for(n=0; n<localStorage.getItem("e5_goo_coins").length; n++)
 {
-	c[n]=localStorage.e5_goo_coins[n].charCodeAt()
+	c[n]=localStorage.getItem("e5_goo_coins")[n].charCodeAt()
 }
 db = [];
 cb = [];
-for (n = 0; n < localStorage.e5_goo_deathsB.length; n++) {
-    db[n] = localStorage.e5_goo_deathsB[n].charCodeAt()
+for (n = 0; n < localStorage.getItem("e5_goo_deathsB").length; n++) {
+    db[n] = localStorage.getItem("e5_goo_deathsB")[n].charCodeAt()
 }
-for (n = 0; n < localStorage.e5_goo_coinsB.length; n++) {
-    cb[n] = localStorage.e5_goo_coinsB[n].charCodeAt()
+for (n = 0; n < localStorage.getItem("e5_goo_coinsB").length; n++) {
+    cb[n] = localStorage.getItem("e5_goo_coinsB")[n].charCodeAt()
 }
 ds = [];
 cs = [];
-for (n = 0; n < localStorage.e5_goo_deathsS.length; n++) {
-    ds[n] = localStorage.e5_goo_deathsS[n].charCodeAt()
+for (n = 0; n < localStorage.getItem("e5_goo_deathsS").length; n++) {
+    ds[n] = localStorage.getItem("e5_goo_deathsS")[n].charCodeAt()
 }
-for (n = 0; n < localStorage.e5_goo_coinsS.length; n++) {
-    cs[n] = localStorage.e5_goo_coinsS[n].charCodeAt()
+for (n = 0; n < localStorage.getItem("e5_goo_coinsS").length; n++) {
+    cs[n] = localStorage.getItem("e5_goo_coinsS")[n].charCodeAt()
 }
